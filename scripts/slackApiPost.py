b8f9deb112d31c1fb52bfe348d64d85ec6745943
Himanshu Gautam <hgautam@ebay.com>
2017-12-01 16:57:32 -0800

    refactored

--- scripts/slackApiPost.py
+++ scripts/slackApiPost.py
@@ -5,7 +5,6 @@ host = sys.argv[1]
 def postSlack(message):
     payload = {'channel': '#scm-outages', 'username': 'monitoringBot', 'text': message, 'icon_emoji': ':cop:'}
     url = 'https://hooks.slack.com/services/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'
-    #r = requests.post(url, data=payload)
     r = requests.post(url, data=json.dumps(payload))
     print(r.text)
 
