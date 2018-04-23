842e9edd960322753f8d8c4ac7ee9c74ec5863cc
Gautam,Himanshu(hgautam) <hgautam@ebay.com>
2018-04-23 16:12:33 -0700

    Create nexusProxyMon.pl

---
+++ scripts/nexusProxyMon.pl
@@ -0,0 +1,19 @@
+use strict;
+use warnings;
+
+my @hostlist = ("ebaycentral04-2275473.lvs02.dev.ebayc3.com", "ebaycentral03-1843722.lvs02.dev.ebayc3.com");
+
+my $commandStatus = 0;
+my @errorList = ();
+foreach my $host (@hostlist) {
+    $commandStatus = system("wget $host --spider --quiet -T 20");
+    if ($commandStatus != 0) {
+        print "$host is dowm\n";
+        push(@errorList, $host)
+    }
+}
+
+if (scalar @errorList > 0 ) {
+    exit 1;
+    #system("echo '@errorList proxy is down' | mail -s 'Warning' hgautam\@ebay.com");
+}
