e9e54cb6d63eb707bdbbeaa7a3ff7b923ccb9b6b
Gautam,Himanshu(hgautam) <hgautam@ebay.com>
2018-04-24 15:57:47 -0700

    Update nexusProxyMon.pl

--- scripts/nexusProxyMon.pl
+++ scripts/nexusProxyMon.pl
@@ -1,9 +1,6 @@
 use strict;
 use warnings;
 
-#my @hostlist = ("ebaycentral04-2275473.lvs02.dev.ebayc3.com", "ebaycentral03-1843722.lvs02.dev.ebayc3.com",
-#               "ebaycentral05-2305230.lvs02.dev.ebayc3.com", "ebaycentral06-2362111.lvs02.dev.ebayc3.com");
-
 my $filename = 'lvsProxyHosts';
 open(my $fh, '<:encoding(UTF-8)', $filename)
   or die "Could not open file '$filename' $!";
@@ -13,8 +10,6 @@ my @errorList = ();
 
 while (my $host = <$fh>) {
   chomp $host;
-  print "$host\n";
-  #foreach my $host (@hostlist) {
   $commandStatus = system("wget $host --spider --quiet -T 20");
   if ($commandStatus != 0) {
         print "$host is dowm\n";
@@ -22,7 +17,6 @@ while (my $host = <$fh>) {
     } else {
         print "$host is up\n";
     }
-#}
 }
 
 
