5a540ce85c7653334b032654a678226d25f94211
Gautam,Himanshu(hgautam) <hgautam@ebay.com>
2018-04-25 09:41:56 -0700

    Update slackApiPost.py

--- scripts/slackApiPost.py
+++ scripts/slackApiPost.py
@@ -6,6 +6,6 @@ def postSlack(message):
     payload = {'channel': '#scm-outages', 'username': 'monitoringBot', 'text': message, 'icon_emoji': ':cop:'}
     url = 'https://hooks.slack.com/services/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'
     r = requests.post(url, data=json.dumps(payload))
-    print(r.text)
+    #print(r.text)
 if __name__ == "__main__":
     postSlack( '<!here> ' + host + ' is down!!')    
