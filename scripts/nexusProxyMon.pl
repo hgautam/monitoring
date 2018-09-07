1587aa37739f6a58dcb459e0e326662b73fe87ff
Gautam,Himanshu(hgautam) <hgautam@ebay.com>
2018-09-07 11:04:50 -0700

    Update nexusProxyMon.pl

--- scripts/nexusProxyMon.pl
+++ scripts/nexusProxyMon.pl
@@ -13,7 +13,7 @@ my @errorList = ();
 while (my $host = <$fh>) {
   chomp $host;
   #$commandStatus = system("wget $host --spider --quiet -T 20");
-  my $appURL = "http://$host";
+  my $appURL = "http://$host/service/local/status";
   print "url is $appURL\n";
   $commandStatus = system("/usr/bin/wget -T 10 -t 3 --spider $appURL > /dev/null 2>&1");
   if ($commandStatus != 0) {
